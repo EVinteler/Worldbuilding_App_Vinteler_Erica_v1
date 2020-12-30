026a91c2fa27e5b1b52bac0cbf656d201882dc23
EVinteler <ericavinteler@yahoo.com>
2020-12-30 16:51:27 +0200

    pas 19 lab 10

--- Worldbuilding_App_Vinteler_Erica_v1/App.xaml.cs
+++ Worldbuilding_App_Vinteler_Erica_v1/App.xaml.cs
@@ -1,16 +1,34 @@
 ﻿using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+using Worldbuilding_App_Vinteler_Erica_v1.Data;
 
 namespace Worldbuilding_App_Vinteler_Erica_v1
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new ListEntryPage());
         }
 
         protected override void OnStart()
