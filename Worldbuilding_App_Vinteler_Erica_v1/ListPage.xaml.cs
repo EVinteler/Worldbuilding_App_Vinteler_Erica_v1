fe9d83f4c1d28cae5a1aa0f819f0e4cb44c85201
EVinteler <ericavinteler@yahoo.com>
2020-12-30 17:22:21 +0200

    base done

--- Worldbuilding_App_Vinteler_Erica_v1/ListPage.xaml.cs
+++ Worldbuilding_App_Vinteler_Erica_v1/ListPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Worldbuilding_App_Vinteler_Erica_v1.Models;
 
 namespace Worldbuilding_App_Vinteler_Erica_v1
 {
@@ -15,6 +16,35 @@ namespace Worldbuilding_App_Vinteler_Erica_v1
         public ListPage()
         {
             InitializeComponent();
+        }
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+        async void OnChooseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ProductPage((ShopList)
+           this.BindingContext)
+            {
+                BindingContext = new Product()
+            });
+
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var shopl = (ShopList)BindingContext;
+
+            listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
         }
     }
 }
