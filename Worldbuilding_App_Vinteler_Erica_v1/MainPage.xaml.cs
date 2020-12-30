30681018700242b821e17bd726d8360798381684
EVinteler <ericavinteler@yahoo.com>
2020-12-30 16:45:48 +0200

    Add project files.

---
+++ Worldbuilding_App_Vinteler_Erica_v1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Worldbuilding_App_Vinteler_Erica_v1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Worldbuilding_App_Vinteler_Erica_v1/Models/ShopList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+
+namespace Worldbuilding_App_Vinteler_Erica_v1.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
